3eee73ecaa7e15ba651540d7ed18778b755778e6
gordun209 <gordun209@hotmail.com>
2023-08-09 19:36:39 +0300

    w

--- init.lua
+++ init.lua
@@ -1,41 +1,42 @@
 require("paq")({
-	"savq/paq-nvim", -- Let Paq manage itself
-	"nvim-lua/plenary.nvim",
-	-- Colortheme
-	{ "catppuccin/nvim",                  as = "catppuccin" },
-	-- Syntax
-	{
-		"nvim-treesitter/nvim-treesitter",
-		run = function()
-			vim.cmd([[:TSUpdateSync]])
-		end,
-	},
-	{ 'VonHeikemen/lsp-zero.nvim',        branch = 'dev-v3' },
-	-- LSP Support
-	{ 'neovim/nvim-lspconfig' },
-	{ 'williamboman/mason.nvim' },
-	{ 'williamboman/mason-lspconfig.nvim' },
-	{ 'folke/neodev.nvim' },
-	-- Autocompletion
-	{ 'hrsh7th/nvim-cmp' },
-	{ 'hrsh7th/cmp-buffer' },
-	{ 'hrsh7th/cmp-path' },
-	{ 'saadparwaiz1/cmp_luasnip' },
-	{ 'hrsh7th/cmp-nvim-lsp' },
-	{ 'hrsh7th/cmp-nvim-lua' },
+    "savq/paq-nvim", -- Let Paq manage itself
+    "nvim-lua/plenary.nvim",
+    -- Colortheme
+    { "catppuccin/nvim",                  as = "catppuccin" },
+    -- Syntax
+    {
+        "nvim-treesitter/nvim-treesitter",
+        run = function()
+            vim.cmd([[:TSUpdateSync]])
+        end,
+    },
+    { 'VonHeikemen/lsp-zero.nvim',        branch = 'dev-v3' },
+    -- LSP Support
+    { 'neovim/nvim-lspconfig' },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+    { 'folke/neodev.nvim' },
+    -- Autocompletion
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'saadparwaiz1/cmp_luasnip' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-nvim-lua' },
 
-	-- Snippets
-	{ 'L3MON4D3/LuaSnip' },
-	{ 'rafamadriz/friendly-snippets' },
-
-
+    -- Snippets
+    { 'L3MON4D3/LuaSnip' },
+    { 'rafamadriz/friendly-snippets' },
 
 
-	{ 'nvim-telescope/telescope.nvim',    tag = '0.1.0' },
-	{ 'onsails/lspkind.nvim' },
 
-	-- Tmux
-	{ 'alexghergh/nvim-tmux-navigation' },
+
+    { 'nvim-telescope/telescope.nvim',    tag = '0.1.0' },
+    { 'onsails/lspkind.nvim' },
+
+    -- Tmux
+    { 'alexghergh/nvim-tmux-navigation' },
+    { "lervag/vimtex" },
 })
 require("core.options")
 require("core.maps")
