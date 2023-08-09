7585276dae8c27bdf9cf7eecd6e0f5e1d2479738
gordun209 <gordun209@hotmail.com>
2023-08-09 11:13:53 +0300

    ab0

--- init.lua
+++ init.lua
@@ -1,20 +1,37 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+require("paq")({
+    "savq/paq-nvim", -- Let Paq manage itself
+    "nvim-lua/plenary.nvim",
+    -- Colortheme
+    { "catppuccin/nvim",                  as = "catppuccin" },
+    -- Syntax
+    {
+        "nvim-treesitter/nvim-treesitter",
+        run = function()
+            vim.cmd([[:TSUpdateSync]])
+        end,
+    },
+    { 'VonHeikemen/lsp-zero.nvim',
+    },
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
-require('lazy').setup("plugins")
-require('core.options')
-require('core.keymaps')
-require('core.autocmds')
-vim.cmd [[ colorscheme catppuccin-mocha ]]
+    -- LSP Support
+    { 'neovim/nvim-lspconfig' },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+
+    -- Autocompletion
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'saadparwaiz1/cmp_luasnip' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-nvim-lua' },
+
+    -- Snippets
+    { 'L3MON4D3/LuaSnip' },
+    { 'rafamadriz/friendly-snippets' },
+
+    { 'nvim-telescope/telescope.nvim',    tag = '0.1.0' },
+})
+require("core.options")
+require("core.maps")
+-- Colorscheme
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,30 +1,31 @@
-vim.opt.number = true
+vim.g.mapleader = " "
+
+vim.opt.nu = true
 vim.opt.relativenumber = true
-vim.opt.scrolloff = 10
 
-vim.opt.splitkeep = "screen"
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-
--- use number of spaces to insert a <Tab>
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
 vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
-
-
-
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-
-
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
