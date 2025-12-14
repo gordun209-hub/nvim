a1dde817b2f3218660343cb494bbdfd077b3ca77
gordun209 <ali-han80@hotmail.com>
2025-12-14 17:08:48 +0300

    remove bloat

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -6,6 +6,6 @@ return {
     config = function()
         require("mini.pairs").setup()
         require("mini.surround").setup()
-        require("mini.statusline").setup()
+        -- require("mini.statusline").setup()
     end,
 }
